4a2aaf6d6f572555f9c0a7ef1e7186f2d6232ec7
Kaz <74264301+givemesleep@users.noreply.github.com>
2022-11-13 14:02:12 +0800

    Add files via upload

---
+++ salary.java
@@ -0,0 +1,64 @@
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Scanner;
+
+public class salary{
+
+public static void main(String[] Args) {       
+
+Scanner scan = new Scanner(System.in);
+
+System.out.print("Name : ");
+String name = scan.nextLine();
+
+System.out.print("Type F if Full Time, Type P if Part Time \nLetter : ");
+String input = scan.nextLine();
+
+//Variables
+
+BigDecimal rph = new BigDecimal("71.25");
+BigDecimal  ot = new BigDecimal("140");
+BigDecimal per = new BigDecimal("1.25");
+
+//Currency Sysmbols
+
+Currency cur = Currency.getInstance("PHP");
+String sym = cur.getSymbol();
+
+// If Statements If, Else If, Else
+
+if (input.equals("F")) {
+
+System.out.print("Monthly Salary : ");
+String ms = scan.nextLine(); 
+
+System.out.println("\n \n \n \n \n");
+
+System.out.println("Name : "+name+"\nMonthly Salary : "+sym+" "+ms);
+}
+
+else if (input.equals("P")){
+System.out.println("Number of Hr spent in OT : ");
+BigDecimal hot = scan.nextBigDecimal();
+
+System.out.println("Number of Hr spent in Work : ");
+BigDecimal hhr = scan.nextBigDecimal();
+
+//compute
+
+BigDecimal one = rph.multiply(per);
+BigDecimal two = one.multiply(hot);
+BigDecimal three = rph.multiply(hhr);
+BigDecimal wages = one.add(three);
+
+System.out.println("\n \n \n \n \n");
+
+System.out.println("Name : "+name+"\nWage : "+sym+" "+wages);
+}
+
+else {
+System.out.println("Please type *F* or *P*");
+}
+
+}
+                                                }
